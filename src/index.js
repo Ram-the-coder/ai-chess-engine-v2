e0f2d8d71604d1987a973b06deba7fe238a44c7b
unknown <ramvard@gmail.com>
2020-04-02 12:19:55 +0530

    chore: setup webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./styles.css";
+alert("Hello from webpack")
---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+	mode: "development",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader",
+				]
+			}
+		]
+	}
+});
